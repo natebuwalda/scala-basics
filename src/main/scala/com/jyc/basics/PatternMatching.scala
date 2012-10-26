c12bb6b54c2047b2a5f2aefd5c5a9bef7aeb48b7
Nate Buwalda <natebuwalda@gmail.com>
2012-10-26 09:06:52 -0500

    Added pattern matching examples

--- src/main/scala/com/jyc/basics/PatternMatching.scala
+++ src/main/scala/com/jyc/basics/PatternMatching.scala
@@ -3,4 +3,41 @@ package com.jyc.basics
 
 class PatternMatching {
 
+  def petsSay(pet: String): String = {
+    pet match {
+      case "Dog" => "Woof"
+      case "Cat" => "Meow"
+      case "Bird" => "Chirp"
+      case _ => "Whoops"
+    }
+  }
+
+  def petTypesSay(pet: Any): String = {
+    pet match {
+      case p: Dog => "Woof"
+      case p: Cat => "Meow"
+      case p: Bird => "Chirp"
+      case _ => "Whoops"
+    }
+  }
+
+  def petCasesSay(pet: NamedPet): String = {
+    pet match {
+      case CaseBird(name) => "%s says Chirp".format(name)
+      case CaseCat(name) => "%s says Meow".format(name)
+      case CaseDog(name) => "%s says Woof".format(name)
+      case CaseSnake(name) => "%s says Hiss".format(name)
+    }
+  }
 }
+
+class Bird
+class Dog
+class Cat
+class Snake
+
+sealed trait NamedPet
+case class CaseBird(name: String) extends NamedPet
+case class CaseDog(name: String) extends NamedPet
+case class CaseCat(name: String) extends NamedPet
+case class CaseSnake(name: String) extends NamedPet
--- src/test/scala/com/jyc/basics/PatternMatchingTest.scala
+++ src/test/scala/com/jyc/basics/PatternMatchingTest.scala
@@ -1,5 +1,30 @@
 package com.jyc.basics
 
-class PatternMatchingTest {
+import org.scalatest.FunSuite
+
+class PatternMatchingTest extends FunSuite {
+
+  private val matcher = new PatternMatching
+
+  test("Patterns match on strings") {
+    assert("Chirp" === matcher.petsSay("Bird"))
+    assert("Meow" === matcher.petsSay("Cat"))
+    assert("Woof" === matcher.petsSay("Dog"))
+    assert("Whoops" === matcher.petsSay("Snake"))
+  }
+
+  test("Patterns match on types") {
+    assert("Chirp" === matcher.petTypesSay(new Bird))
+    assert("Meow" === matcher.petTypesSay(new Cat))
+    assert("Woof" === matcher.petTypesSay(new Dog))
+    assert("Whoops" === matcher.petTypesSay(new Snake))
+  }
+
+  test("Case classes are more fun") {
+    assert("Polly says Chirp" === matcher.petCasesSay(CaseBird("Polly")))
+    assert("Garfield says Meow" === matcher.petCasesSay(CaseCat("Garfield")))
+    assert("Lassie says Woof" === matcher.petCasesSay(CaseDog("Lassie")))
+    assert("Kaa says Hiss" === matcher.petCasesSay(CaseSnake("Kaa")))
+  }
 
 }
